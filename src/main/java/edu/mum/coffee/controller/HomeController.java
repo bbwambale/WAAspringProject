8d70911397a50713a9e9e40d4c35f8c3d33ff44b
bbwambale <basiyonah033@gmail.com>
2018-06-07 19:28:58 -0500

    Welcome Page Person registration page Authentication test done Connection to database test done

--- src/main/java/edu/mum/coffee/controller/HomeController.java
+++ src/main/java/edu/mum/coffee/controller/HomeController.java
@@ -1,22 +1,25 @@
+
 package edu.mum.coffee.controller;
 
+import edu.mum.coffee.domain.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@GetMapping({"/", "/index", "/home"})
-	public String homePage() {
+	public String homePage(Model model) {
+
+		model.addAttribute("person", new Person());
 		return "home";
 	}
 
 	@GetMapping({"/secure"})
-	public String securePage()
-
-
-	{
+	public String securePage() {
 		return "secure";
 	}
+
 
 }
--- src/main/java/edu/mum/coffee/domain/Person.java
+++ src/main/java/edu/mum/coffee/domain/Person.java
@@ -21,6 +21,8 @@ public class Person {
 	private Address address;
 	private String phone;
 	private boolean enable;
+	private String password;
+
 
 	public long getId() {
 		return id;
@@ -74,4 +76,11 @@ public class Person {
 		this.phone = phone;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
